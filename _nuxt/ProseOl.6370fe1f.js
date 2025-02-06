06e2388375d4ad6f3a0cc7427d6af9c21f743f0a
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-06 17:33:55 +0800

    Updates

---
+++ _nuxt/ProseOl.6370fe1f.js
@@ -0,0 +1,1 @@
+import{s as o,o as r,v as s,r as t}from"./entry.0f0e7016.js";const n={};function c(e,l){return r(),s("ol",null,[t(e.$slots,"default")])}const f=o(n,[["render",c]]);export{f as default};
---
+++ _nuxt/html.7e39341f.js
@@ -0,0 +1,1 @@
+import{W as o}from"./entry.0f0e7016.js";const s=t=>{const{t:e}=o();return`${t} | ${e("app_title")}`},a=t=>{const{t:e}=o(),l=e(t);return s(l)};export{a as l,s as t};
